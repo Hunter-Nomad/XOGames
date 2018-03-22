986ddb7952346cae3fe317bd8a5b53250d21d19c
Hunter-Nomad <rqj.polinovsky@gmail.com>
2018-03-22 11:44:59 +0200

    Изменил алгоритм проверки выигрышных комбинаций

--- XOGameBoard.sol
+++ XOGameBoard.sol
@@ -4,33 +4,51 @@ contract XOGameBoard{
     
     uint8[9] private board; // игровая доска
     uint8 private step; // текущий номер хода в игре
-    address player1;
-    address player2;
     uint8 res;
      
-    function XOGameBoard(address _player1) public {
-        player1 = _player1;
+    function XOGameBoard() public {
         step = 0;
     }
   
-    function joinPlayer(address _player2) public{
-        player2 = _player2;
-    }
-  
-    function move(uint8 x, uint8 y, uint8 chip) public{
+    function move(uint8 x, uint8 y, uint8 chip) public returns(uint8){
         step += 1;
-        if(x == 1 && y == 1){ board[0] = chip;}
-        if(x == 1 && y == 2){ board[1] = chip;}
-        if(x == 1 && y == 3){ board[2] = chip;}
-        if(x == 2 && y == 1){ board[3] = chip;}
-        if(x == 2 && y == 2){ board[4] = chip;}
-        if(x == 2 && y == 3){ board[5] = chip;}
-        if(x == 3 && y == 1){ board[6] = chip;}
-        if(x == 3 && y == 2){ board[7] = chip;}
-        if(x == 3 && y == 3){ board[8] = chip;}
-        
-       
-        // res = checkBoard();
+        if(x == 1 && y == 1){ 
+            board[0] = chip;
+            if(step >= 5){ return cell0(chip); }
+        }
+        if(x == 1 && y == 2){ 
+            board[1] = chip;
+            if(step >= 5){ return cell1(chip); }
+        }
+        if(x == 1 && y == 3){ 
+            board[2] = chip;
+            if(step >= 5){ return cell2(chip); }
+        }
+        if(x == 2 && y == 1){ 
+            board[3] = chip;
+            if(step >= 5){ return cell3(chip); }
+        }
+        if(x == 2 && y == 2){ 
+            board[4] = chip;
+            if(step >= 5){ return cell4(chip); }
+        }
+        if(x == 2 && y == 3){ 
+            board[5] = chip;
+            if(step >= 5){ return cell5(chip); }
+        }
+        if(x == 3 && y == 1){ 
+            board[6] = chip;
+            if(step >= 5){ return cell6(chip); }
+        }
+        if(x == 3 && y == 2){ 
+            board[7] = chip;
+            if(step >= 5){ return cell7(chip); }
+        }
+        if(x == 3 && y == 3){ 
+            board[8] = chip;
+            if(step >= 5){ return cell8(chip); }
+        }
+        if(step == 9){ return 3; } else { return 0;}
     }
      
   
@@ -38,40 +56,53 @@ contract XOGameBoard{
     // 3 4 5
     // 6 7 8
     
-    function winPlayer1() public view returns(uint8){
-        if(board[0] == 1 && board[1] == 1 && board[2] == 1){return 1;}
-        if(board[3] == 1 && board[4] == 1 && board[5] == 1){return 1;}
-        if(board[6] == 1 && board[7] == 1 && board[8] == 1){return 1;}
-        if(board[0] == 1 && board[3] == 1 && board[6] == 1){return 1;}
-        if(board[1] == 1 && board[4] == 1 && board[7] == 1){return 1;}
-        if(board[2] == 1 && board[5] == 1 && board[8] == 1){return 1;}
-        if(board[0] == 1 && board[4] == 1 && board[8] == 1){return 1;}
-        if(board[2] == 1 && board[4] == 1 && board[6] == 1){return 1;}
+    function cell0(uint8 move) public view returns(uint8){
+        if(board[0] == move && board[1] == move && board[2] == move){return move;}
+        if(board[0] == move && board[3] == move && board[6] == move){return move;}
+        if(board[0] == move && board[4] == move && board[8] == move){return move;}
+    }
+    function cell1(uint8 move) public view returns(uint8){
+        if(board[0] == move && board[1] == move && board[2] == move){return move;}
+        if(board[1] == move && board[4] == move && board[7] == move){return move;}
+    }
+    function cell2(uint8 move) public view returns(uint8){
+        if(board[0] == move && board[1] == move && board[2] == move){return move;}
+        if(board[2] == move && board[5] == move && board[8] == move){return move;}
+        if(board[2] == move && board[4] == move && board[6] == move){return move;}
+    }
+    function cell3(uint8 move) public view returns(uint8){
+        if(board[0] == move && board[3] == move && board[6] == move){return move;}
+        if(board[3] == move && board[4] == move && board[5] == move){return move;}
+    }
+    function cell4(uint8 move) public view returns(uint8){
+        if(board[1] == move && board[4] == move && board[7] == move){return move;}
+        if(board[3] == move && board[4] == move && board[5] == move){return move;}
+        if(board[0] == move && board[4] == move && board[8] == move){return move;}
+        if(board[2] == move && board[4] == move && board[6] == move){return move;}
+    }
+    function cell5(uint8 move) public view returns(uint8){
+        if(board[2] == move && board[5] == move && board[8] == move){return move;}
+        if(board[3] == move && board[4] == move && board[5] == move){return move;}
+    }
+    function cell6(uint8 move) public view returns(uint8){
+        if(board[0] == move && board[3] == move && board[6] == move){return move;}
+        if(board[2] == move && board[4] == move && board[6] == move){return move;}
+        if(board[6] == move && board[7] == move && board[8] == move){return move;}
+    }
+    function cell7(uint8 move) public view returns(uint8){
+        if(board[1] == move && board[4] == move && board[7] == move){return move;}
+        if(board[6] == move && board[7] == move && board[8] == move){return move;}
+    }
+    function cell8(uint8 move) public view returns(uint8){
+        if(board[2] == move && board[5] == move && board[8] == move){return move;}
+        if(board[0] == move && board[4] == move && board[8] == move){return move;}
+        if(board[6] == move && board[7] == move && board[8] == move){return move;}
     }
     
-    function winPlayer2() public view returns(uint8){
-        if(board[0] == 2 && board[1] == 2 && board[2] == 2){return 2;}
-        if(board[3] == 2 && board[4] == 2 && board[5] == 2){return 2;}
-        if(board[6] == 2 && board[7] == 2 && board[8] == 2){return 2;}
-        if(board[0] == 2 && board[3] == 2 && board[6] == 2){return 2;}
-        if(board[1] == 2 && board[4] == 2 && board[7] == 2){return 2;}
-        if(board[2] == 2 && board[5] == 2 && board[8] == 2){return 2;}
-        if(board[0] == 2 && board[4] == 2 && board[8] == 2){return 2;}
-        if(board[2] == 2 && board[4] == 2 && board[6] == 2){return 2;}
-    }
+   /* 
     
-    function checkBoard() public view returns(uint8){
-        if(winPlayer1() == 1){ return 1;}
-        if(winPlayer2() == 2){ return 2;}
-        if(step == 9){
-            return 3;
-        } else {
-            return 0;
-        }
-    }
-     /*
-    function getBoard() public returns(uint8[9]){
-        return board;
-    }
+    // function getBoard() public returns(uint8[9]){
+    //     return board;
+    // }
     */
 }
--- XOGames.sol
+++ XOGames.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.19;
 
 import "./XOGameBoard.sol";
 
@@ -7,34 +7,22 @@ contract XOGames{
     address owner; // владелец контракта
     address[] public gameBoards;
     uint8[9] board;
-    byte O; // фишка "нолик"
-    byte X; // фишка "крестик"
-    uint8 stepEnd = 9; // последний ход в игре //??????
     uint public gameCount; // число активных игр
     
     // статусы игры
     enum StatusGame {
         GAME_OVER, // игра окончена
         GAME_EXPECTED, // игра готова к старту, ожидается второй игрок
-        GAME_START, // игра стартовала
-        PLAYER1_MOVE, // ход первого игрока
-        PLAYER2_MOVE, // ход второго игрока
-        PLAYER1_WIN, // выйграл первый игрок
-        PLAYER2_WIN, // выйграл второй игрок
-        PLAYERS_DRAW // ничья
+        GAME_START // игра стартовала
     }
-    // StatusGame statusGame;
-    StatusGame constant defaultStatusGame = StatusGame.GAME_EXPECTED; // статус по умолчанию
-    
+   
      // структура игрока
     struct Player{
         address player;
-        byte chip; // "X" или "0"
     }
     
     // структура игры
     struct Game{
-        // address gameBoard;
         Player player1; // игрок 1, он инициирует игру
         Player player2; // игрок 2, он присоединяется к игре
         uint8 statusGame; // статус игры
@@ -54,6 +42,10 @@ contract XOGames{
     event JoinToGame(string str, address _address, string rate, uint _value);
     event Board(uint8 l1, uint8 l2, uint8 l3, uint8 l4, uint8 l5, uint8 l6, uint8 l7, uint8 l8, uint8 l9);
     event CheckBoard(uint8 u);
+    event Move(string msg);
+    event MSG(string msg);
+    event Win(string msg);
+    
     
     // модификатор прав владельуа контракта
     modifier isOwner {
@@ -65,67 +57,86 @@ contract XOGames{
     function XOGames() public {
         owner = msg.sender;
         gameCount = 0;
-        X = "X"; // 0x58
-        O = "O"; // 0x4f
     }
     
     // инициализация/создание игры
     function createGame() public payable returns(XOGameBoard gameBoard){
         if(msg.value == 0){
-            emit ErrorValue("'value' can not be zero", 0);
+            ErrorValue("'value' can not be zero", 0);
             return;
         }
         if(players[msg.sender] == 1){
-            emit ErrorAddress("You have already created a game");
+            ErrorAddress("You have already created a game");
             return;
         }
-        gameBoard = new XOGameBoard(msg.sender);
+        gameBoard = new XOGameBoard();
         gameBoards.push(gameBoard);
-        players[msg.sender] = 1;
+        players[msg.sender] = 1; // игрок готов к игре
         games[gameBoard].player1.player = msg.sender;
-        // games[gameBoard].player1.chip = X;
-        games[gameBoard].statusGame = uint8(defaultStatusGame);
-        games[gameBoard].balance = msg.value;
-        emit CreateGame("New game create in address", gameBoard, "rate", msg.value);
+        games[gameBoard].statusGame = uint8(StatusGame.GAME_EXPECTED); // статус игры "в ожитании"
+        games[gameBoard].balance = msg.value; // запись ставки первого игрока
+        CreateGame("New game create in address", gameBoard, "rate", msg.value);
         return gameBoard;
     }
     
+    // присоединиться к игре второму игроку
     function joinToGame(XOGameBoard gameBoard) public payable{
         if(gameBoard == address(0x0)){
-            emit ErrorAddress("Error game address!"); 
+            ErrorAddress("Error game address!"); 
             // return;
             revert();
         }
+        if(players[msg.sender] == 1){
+            ErrorAddress("You have join to game");
+            revert();
+            // return;
+        }
         if(msg.value != games[gameBoard].balance){
-            emit ErrorValue("Invalid value. The value must be", games[gameBoard].balance);
+            ErrorValue("Invalid value. The value must be", games[gameBoard].balance);
             revert();
         }
-        if(games[gameBoard].statusGame == uint8(defaultStatusGame)){
-            players[msg.sender] = 1;
+        if(games[gameBoard].statusGame == uint8(StatusGame.GAME_EXPECTED)){
+            players[msg.sender] = 1; // игрок готов к игре
             games[gameBoard].player2.player = msg.sender; 
-            // games[gameAddress].player2.chip = O;
             games[gameBoard].statusGame = uint8(StatusGame.GAME_START);
-            games[gameBoard].balance += msg.value;
-            gameBoard.joinPlayer(msg.sender);
-            emit JoinToGame("You join to game. Address", gameBoard, "rate", msg.value);
+            games[gameBoard].balance += msg.value; // добавление ставки второго игрока
+            JoinToGame("You join to game. Address", gameBoard, "rate", msg.value);
         } else {
-            emit ErrorAddress("The game is started!"); 
+            ErrorAddress("The game is started!"); 
         }
     }
     
-    function move(XOGameBoard gameBoard, uint8 x, uint8 y) public{
-        gameBoard.move(x, y, 1);
+    function move(uint8 x, uint8 y, XOGameBoard gameBoard) public{
+        uint8 _player;
+        uint8 status;
+        if(gameBoard == address(0x0)){
+            ErrorAddress("Error game address!");
+            return;
+        }
+        if(games[gameBoard].player1.player == msg.sender){
+            _player = 1;
+            Move("Plaeyr 1 move");
+        }
+        if(games[gameBoard].player2.player == msg.sender){
+            _player = 2;
+            Move("Plaeyr 2 move");
+        }
+        // status = gameBoard.move(x, y, _player);
+        
+        // if(gameBoard.move(x, y, _player) == 1){
+        //     Win("Player 1 is WIN!");
+        // }
+        // if(gameBoard.move(x, y, _player) == 2){
+        //     Win("Player 2 is WIN!");
+        // }
+        // if(gameBoard.move(x, y, _player) == 3){
+        //     Win("In game is DRAW!");
+        // }
     }
     
-    function getBoard(XOGameBoard gameBoard) public{
-        board = gameBoard.getBoard();
-        emit Board(board[0], board[1], board[2], board[3], (board[4]), board[5], board[6], (board[7]), board[8]);
-        // emit Board(board[3], (board[4]), board[5]);
-        // emit Board(board[6], (board[7]), board[8]);
-    }
-    
-    function getCheckBoard(XOGameBoard gameBoard) public{
-        CheckBoard(gameBoard.checkBoard());
-    }
+    // function getBoard(XOGameBoard gameBoard) public{
+    //     board = gameBoard.getBoard();
+    //     Board(board[0], board[1], board[2], board[3], (board[4]), board[5], board[6], (board[7]), board[8]);
+    // }
     
 }
